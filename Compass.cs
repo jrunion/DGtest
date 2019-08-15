b5cfb45ada3b5c0c817203a248dd3d73f3e2d0d3
jrunion <jrunion@mail.csuchico.edu>
2019-08-15 15:03:52 -0700

    Add files via upload

---
+++ Compass.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject text;
+    Text degree;
+    
+    private float rotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GameObject.Find("Degrees");
+        player = GameObject.Find("player");
+        degree = text.GetComponent<Text>();
+        degree.text = "0";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation = player.transform.eulerAngles.y;
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, rotation * -1);
+
+        DegreeText();
+    }
+
+    void DegreeText()
+    {
+        if(transform.eulerAngles.z >=45)
+            degree.text = Mathf.Round(360+(transform.eulerAngles.z -45f)*-1).ToString();
+        else
+            degree.text = Mathf.Round(360+(transform.eulerAngles.z + 315f)*-1).ToString();
+
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    private float speed = 10f;
+
+    float mouseInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        this.transform.position += Movement * speed * UnityEngine.Time.deltaTime;
+
+        mouseInput = Input.GetAxis("Mouse X");
+        Vector3 lookhere = new Vector3(0, mouseInput, 0);
+        transform.Rotate(lookhere);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 up = transform.TransformDirection(Vector3.up);
+            GetComponent<Rigidbody>().AddForce(up * 5, ForceMode.Impulse);
+        }
+    }
+}
