0ccce727ff2019e3b9aca2feaf3202dbb0a0c6e0
jrunion <jrunion@mail.csuchico.edu>
2019-08-18 15:05:07 -0700

    Add files via upload

---
+++ HeatMissile.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatMissile : MonoBehaviour
+{
+
+    GameObject player;
+    Vector3 targetPos;
+    float distance;
+
+    void Start()
+    {
+        player = GameObject.Find("player");
+    }
+
+    void Update()
+    {
+      
+
+        targetPos = player.transform.position;
+        distance = Vector3.Distance(targetPos,transform.position);
+
+
+        if (distance > 10)
+            transform.position += (targetPos - transform.position) / 20;
+        else
+            transform.position += new Vector3(((targetPos.x) - transform.position.x) / 20, transform.position.y/30 , ((targetPos.z) - transform.position.z) / 20);
+    }
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Hit Player");
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Debug.Log("Missed Player");
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject missile;
+
+    float timer = 3;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer<=0.0f)
+        {
+            if (!GameObject.Find("missile"))
+                Instantiate(missile);
+
+            timer = 2;
+        }
+
+
+
+     
+    }
+}
